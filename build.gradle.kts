c2f8d6a8435ff9092fdfd30a946bd6bd050e9f82
iseki <admin@iseki.space>
2024-11-16 13:21:26 +0800

    chore(maven): prepare for publication

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,10 +3,12 @@ plugins {
     jacoco
     id("org.jetbrains.kotlinx.binary-compatibility-validator") version "0.16.3"
     kotlin("jvm") version "2.0.20"
+    `maven-publish`
+    signing
 }
 
 group = "space.iseki.pefile"
-version = "1.0-SNAPSHOT"
+version = "0.1-SNAPSHOT"
 
 repositories {
     mavenCentral()
@@ -32,12 +34,25 @@ java {
     }
     sourceCompatibility = JavaVersion.VERSION_17
     targetCompatibility = JavaVersion.VERSION_17
+    withSourcesJar()
 }
 
 tasks.jar {
     into("META-INF/versions/22") {
         from(java22.output)
     }
+    from("LICENSE", "NOTICE")
+    manifest {
+        attributes["Multi-Release"] = "true"
+    }
+}
+
+tasks.getByName("sourcesJar") {
+    this as Jar
+    into("META-INF/versions/22") {
+        from(java22.allSource)
+    }
+    from("LICENSE", "NOTICE")
     manifest {
         attributes["Multi-Release"] = "true"
     }
@@ -85,4 +100,67 @@ tasks.check {
     dependsOn("testJava22", "test")
 }
 
+publishing {
+    repositories {
+        maven {
+            name = "Central"
+            afterEvaluate {
+                url = if (version.toString().endsWith("SNAPSHOT")) {
+                    // uri("https://s01.oss.sonatype.org/content/repositories/snapshots")
+                    uri("https://oss.sonatype.org/content/repositories/snapshots")
+                } else {
+                    // uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2")
+                    uri("https://oss.sonatype.org/service/local/staging/deploy/maven2")
+                }
+            }
+            credentials {
+                username = properties["ossrhUsername"]?.toString() ?: System.getenv("OSSRH_USERNAME")
+                password = properties["ossrhPassword"]?.toString() ?: System.getenv("OSSRH_PASSWORD")
+            }
+        }
+    }
 
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+            artifact(emptyJavadoc)
+        }
+        withType<MavenPublication> {
+            pom {
+                val projectUrl = "https://github.com/iseki0/PEFile"
+                name = "PEFile"
+                description = "A library for PE file"
+                url = projectUrl
+                licenses {
+                    license {
+                        name = "Apache-2.0"
+                        url = "https://www.apache.org/licenses/LICENSE-2.0"
+                    }
+                }
+                developers {
+                    developer {
+                        id = "iseki0"
+                        name = "iseki zero"
+                        email = "iseki@iseki.space"
+                    }
+                }
+                inceptionYear = "2024"
+                scm {
+                    connection = "scm:git:$projectUrl.git"
+                    developerConnection = "scm:git:$projectUrl.git"
+                    url = projectUrl
+                }
+                issueManagement {
+                    system = "GitHub"
+                    url = "$projectUrl/issues"
+                }
+            }
+        }
+    }
+
+}
+
+signing {
+    useGpgCmd()
+    sign(publishing.publications)
+}
